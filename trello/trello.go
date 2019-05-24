7826dc8f454f34655219bd1a5973a1a8a065388a
ht68rohit <rohits@heaptrace.com>
2019-05-24 16:52:49 +0530

    updated code

--- trello/trello.go
+++ trello/trello.go
@@ -17,8 +17,6 @@ type TrelloArgs struct {
 //GetCards trello
 func GetCards(responseWriter http.ResponseWriter, request *http.Request) {
 
-	//apiKey := "60eab9d853e88b36ae0047ade46b8816"
-	//token := "a8ea88ad56eeb88ec495a0470024a553382f7952ae17ba595795b1a1096c6569"
 	var apiKey = os.Getenv("API_KEY")
 	var token = os.Getenv("ACCESS_TOKEN")
 
